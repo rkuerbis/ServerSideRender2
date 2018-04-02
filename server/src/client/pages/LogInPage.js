060aad1948e57c34fb1fe887ce5de660e2043569
rkuerbis <rkuerbis@hotmail.com>
2018-04-02 03:12:31 -0700

    Update Login
    Updated Login page description.

--- server/src/client/pages/LogInPage.js
+++ server/src/client/pages/LogInPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LogInPage = () => {
   return (
     <div className="center-align" style={{ marginTop: '200px' }}>
-      <h5>Please use Login button at upper right hand corner of page:</h5>
+      <h5>Please use <b>Login</b> button at upper right hand corner of page:</h5>
       <p>Use existing Google account to log in to this application.</p>
     </div>
   );
